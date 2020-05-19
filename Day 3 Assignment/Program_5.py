b35980efdb4d150673f87f6984984a9261f75f58
imshivamrai <65527794+imshivamrai@users.noreply.github.com>
2020-05-19 10:12:56 +0530

    Create Program_5.py

---
+++ Day 3 Assignment/Program_5.py
@@ -0,0 +1,3 @@
+list1 = list(input('Enter elements of a list 1 ').split())
+list2 = list(input('Enter elements of a list 2 ').split())
+print('Intersection ',list(set(list1).intersection(set(list2))))
