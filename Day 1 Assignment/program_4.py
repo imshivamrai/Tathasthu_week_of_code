08bc74a77141c1068b10982a5e98f0ec83121e78
imshivamrai <65527794+imshivamrai@users.noreply.github.com>
2020-05-19 10:02:36 +0530

    Create program_4.py

---
+++ Day 1 Assignment/program_4.py
@@ -0,0 +1,7 @@
+c=(float)(input('Enter the cost price'))
+s=(float)(input('Enter the selling price'))
+profit=s-c
+print('Profit from this sell',profit)
+np=profit+(profit*0.05)
+ns=np+c
+print('new sell price',ns)
